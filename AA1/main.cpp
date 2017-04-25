ee349da2b32434a4dbadd9ea2b5388dba80fb628
Yang Liangde <Kieren@yangs-macbook-pro.local>
2017-04-25 13:23:54 +1000

    Initial Commit

---
+++ AA1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AA1
+//
+//  Created by Yang Liangde on 25/4/17.
+//  Copyright © 2017 Yang Liangde. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
